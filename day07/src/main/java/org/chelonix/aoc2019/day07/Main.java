d568d5d88b8d24a89a4dd01ce3133fb15296e8d6
Jean-Christophe Sirot <jean-christophe.sirot@docker.com>
2019-12-07 13:03:35 +0100

    Code cleanup
    Signed-off-by: Jean-Christophe Sirot <jean-christophe.sirot@docker.com>

--- day07/src/main/java/org/chelonix/aoc2019/day07/Main.java
+++ day07/src/main/java/org/chelonix/aoc2019/day07/Main.java
@@ -1,9 +1,17 @@
 package org.chelonix.aoc2019.day07;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 import java.util.Set;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.function.IntConsumer;
+import java.util.function.IntSupplier;
 import java.util.stream.Collectors;
 
 public class Main {
@@ -24,5 +32,142 @@ public class Main {
             }
         }
         return permutations;
+    }
+
+    public static int maxAmpOutput(String path) throws IOException {
+        int max = Integer.MIN_VALUE;
+        List<List<Integer>> permutations = Main.permutations(new HashSet<>(Arrays.asList(0, 1, 2, 3, 4)));
+        for (List<Integer> p: permutations) {
+
+            IntCode ampA = new IntCode(path);
+            int outA = runAmp(ampA, p.get(0), 0);
+
+            IntCode ampB = new IntCode(path);
+            int outB = runAmp(ampB, p.get(1), outA);
+
+            IntCode ampC = new IntCode(path);
+            int outC = runAmp(ampC, p.get(2), outB);
+
+            IntCode ampD = new IntCode(path);
+            int outD = runAmp(ampD, p.get(3), outC);
+
+            IntCode ampE = new IntCode(path);
+            int outE = runAmp(ampE, p.get(4), outD);
+
+            if (outE > max) {
+                max = outE;
+            }
+        }
+        return max;
+    }
+
+    private static int runAmp(IntCode amp, int i1, int i2)  {
+        amp.setInput(new AmpInput(i1, i2));
+        AmpOutput output = new AmpOutput();
+        amp.setOutput(output);
+        amp.execute();
+        return output.getAsInt();
+    }
+
+    public static int maxAmpOutputWithFeeback(String path) throws IOException {
+        List<List<Integer>> permutations = Main.permutations(new HashSet<>(Arrays.asList(5, 6, 7, 8, 9)));
+        int max = Integer.MIN_VALUE;
+        for (List<Integer> p: permutations) {
+            AmpBridge bridgeEA = new AmpBridge(p.get(0), 0);
+            AmpBridge bridgeAB = new AmpBridge(p.get(1));
+            AmpBridge bridgeBC = new AmpBridge(p.get(2));
+            AmpBridge bridgeCD = new AmpBridge(p.get(3));
+            AmpBridge bridgeDE = new AmpBridge(p.get(4));
+            Thread tA = runAmpFeeback(new IntCode(path), bridgeEA, bridgeAB);
+            Thread tB = runAmpFeeback(new IntCode(path), bridgeAB, bridgeBC);
+            Thread tC = runAmpFeeback(new IntCode(path), bridgeBC, bridgeCD);
+            Thread tD = runAmpFeeback(new IntCode(path), bridgeCD, bridgeDE);
+            Thread tE = runAmpFeeback(new IntCode(path), bridgeDE, bridgeEA);
+
+            tA.start();
+            tB.start();
+            tC.start();
+            tD.start();
+            tE.start();
+
+            try {
+                tA.join();
+                tB.join();
+                tC.join();
+                tD.join();
+                tD.join();
+            } catch (InterruptedException ie) {
+                ie.printStackTrace();
+            }
+
+            int out = bridgeEA.getAsInt();
+            if (out > max) {
+                max = out;
+            }
+        }
+
+        return max;
+    }
+
+    private static Thread runAmpFeeback(IntCode amp, IntSupplier input, IntConsumer output) {
+        Thread t = new Thread(() -> amp.execute());
+        amp.setInput(input);
+        amp.setOutput(output);
+        return t;
+    }
+
+    private static class AmpInput implements IntSupplier {
+        private int[] values;
+        private int index = 0;
+
+        AmpInput(int... values) {
+            this.values = values;
+        }
+
+        @Override
+        public int getAsInt() {
+            return values[index++];
+        }
+    }
+
+    private static class AmpOutput implements IntConsumer {
+        private Queue<Integer> output = new LinkedList<>();
+
+        AmpOutput() {}
+
+        @Override
+        public void accept(int value) {
+            output.add(value);
+        }
+
+        public int getAsInt() {
+            return output.remove();
+        }
+    }
+
+    private static class AmpBridge implements IntSupplier, IntConsumer {
+
+        private BlockingQueue<Integer> queue = new LinkedBlockingQueue<>();
+
+        public AmpBridge(int... initialInputs) {
+            for (int x: initialInputs) {
+                queue.add(x);
+            }
+        }
+
+        @Override
+        public void accept(int value) {
+            queue.add(value);
+        }
+
+        @Override
+        public int getAsInt() {
+            try {
+                return queue.take();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                return -1;
+            }
+        }
     }
 }
--- day07/src/test/java/org/chelonix/aoc2019/day07/MainTest.java
+++ day07/src/test/java/org/chelonix/aoc2019/day07/MainTest.java
@@ -34,60 +34,13 @@ public class MainTest {
             "org/chelonix/aoc2019/day07/test3.txt, 65210",
     })
     public void testPart1(String path, int expected) throws IOException {
-        List<List<Integer>> permutations = Main.permutations(new HashSet<>(Arrays.asList(0, 1, 2, 3, 4)));
-        int max = Integer.MIN_VALUE;
-        for (List<Integer> p: permutations) {
-
-            IntCode ampA = new IntCode(path);
-            int outA = runAmp(ampA, p.get(0), 0);
-
-            IntCode ampB = new IntCode(path);
-            int outB = runAmp(ampB, p.get(1), outA);
-
-            IntCode ampC = new IntCode(path);
-            int outC = runAmp(ampC, p.get(2), outB);
-
-            IntCode ampD = new IntCode(path);
-            int outD = runAmp(ampD, p.get(3), outC);
-
-            IntCode ampE = new IntCode(path);
-            int outE = runAmp(ampE, p.get(4), outD);
-
-            if (outE > max) {
-                max = outE;
-            }
-        }
-
+        int max = Main.maxAmpOutput(path);
         assertThat(max).isEqualTo(expected);
     }
 
     @Test
     public void part1() throws IOException {
-        String path = "org/chelonix/aoc2019/day07/input.txt";
-        List<List<Integer>> permutations = Main.permutations(new HashSet<>(Arrays.asList(0, 1, 2, 3, 4)));
-        int max = Integer.MIN_VALUE;
-        for (List<Integer> p: permutations) {
-
-            IntCode ampA = new IntCode(path);
-            int outA = runAmp(ampA, p.get(0), 0);
-
-            IntCode ampB = new IntCode(path);
-            int outB = runAmp(ampB, p.get(1), outA);
-
-            IntCode ampC = new IntCode(path);
-            int outC = runAmp(ampC, p.get(2), outB);
-
-            IntCode ampD = new IntCode(path);
-            int outD = runAmp(ampD, p.get(3), outC);
-
-            IntCode ampE = new IntCode(path);
-            int outE = runAmp(ampE, p.get(4), outD);
-
-            if (outE > max) {
-                max = outE;
-            }
-        }
-        System.out.println(max);
+        System.out.println(Main.maxAmpOutput("org/chelonix/aoc2019/day07/input.txt"));
     }
 
     @ParameterizedTest
@@ -96,176 +49,13 @@ public class MainTest {
             "org/chelonix/aoc2019/day07/test5.txt, 139629729",
     })
     public void testPart2(String path, int expected) throws IOException {
-        List<List<Integer>> permutations = Main.permutations(new HashSet<>(Arrays.asList(5, 6, 7, 8, 9)));
-        int max = Integer.MIN_VALUE;
-        for (List<Integer> p: permutations) {
-            AmpBridge bridgeEA = new AmpBridge(p.get(0), 0);
-            AmpBridge bridgeAB = new AmpBridge(p.get(1));
-            AmpBridge bridgeBC = new AmpBridge(p.get(2));
-            AmpBridge bridgeCD = new AmpBridge(p.get(3));
-            AmpBridge bridgeDE = new AmpBridge(p.get(4));
-            IntCode ampA = new IntCode(path);
-            Thread tA = new Thread(() -> ampA.execute(), "AMPA");
-            ampA.setInput(bridgeEA);
-            ampA.setOutput(bridgeAB);
-            IntCode ampB = new IntCode(path);
-            Thread tB = new Thread(() -> ampB.execute(), "AMPB");
-            ampB.setInput(bridgeAB);
-            ampB.setOutput(bridgeBC);
-            IntCode ampC = new IntCode(path);
-            Thread tC = new Thread(() -> ampC.execute(), "AMPC");
-            ampC.setInput(bridgeBC);
-            ampC.setOutput(bridgeCD);
-            IntCode ampD = new IntCode(path);
-            Thread tD = new Thread(() -> ampD.execute(), "AMPD");
-            ampD.setInput(bridgeCD);
-            ampD.setOutput(bridgeDE);
-            IntCode ampE = new IntCode(path);
-            Thread tE = new Thread(() -> ampE.execute(), "AMPE");
-            ampE.setInput(bridgeDE);
-            ampE.setOutput(bridgeEA);
-
-            tA.start();
-            tB.start();
-            tC.start();
-            tD.start();
-            tE.start();
-
-            try {
-                tA.join();
-                tB.join();
-                tC.join();
-                tD.join();
-                tD.join();
-            } catch (InterruptedException ie) {
-                ie.printStackTrace();
-            }
-
-            int out = bridgeEA.getAsInt();
-            if (out > max) {
-                max = out;
-            }
-        }
-
+        int max = Main.maxAmpOutputWithFeeback(path);
         assertThat(max).isEqualTo(expected);
     }
 
     @Test
     public void part2() throws IOException {
-        String path = "org/chelonix/aoc2019/day07/input.txt";
-        List<List<Integer>> permutations = Main.permutations(new HashSet<>(Arrays.asList(5, 6, 7, 8, 9)));
-        int max = Integer.MIN_VALUE;
-        for (List<Integer> p: permutations) {
-            AmpBridge bridgeEA = new AmpBridge(p.get(0), 0);
-            AmpBridge bridgeAB = new AmpBridge(p.get(1));
-            AmpBridge bridgeBC = new AmpBridge(p.get(2));
-            AmpBridge bridgeCD = new AmpBridge(p.get(3));
-            AmpBridge bridgeDE = new AmpBridge(p.get(4));
-            IntCode ampA = new IntCode(path);
-            Thread tA = new Thread(() -> ampA.execute(), "AMPA");
-            ampA.setInput(bridgeEA);
-            ampA.setOutput(bridgeAB);
-            IntCode ampB = new IntCode(path);
-            Thread tB = new Thread(() -> ampB.execute(), "AMPB");
-            ampB.setInput(bridgeAB);
-            ampB.setOutput(bridgeBC);
-            IntCode ampC = new IntCode(path);
-            Thread tC = new Thread(() -> ampC.execute(), "AMPC");
-            ampC.setInput(bridgeBC);
-            ampC.setOutput(bridgeCD);
-            IntCode ampD = new IntCode(path);
-            Thread tD = new Thread(() -> ampD.execute(), "AMPD");
-            ampD.setInput(bridgeCD);
-            ampD.setOutput(bridgeDE);
-            IntCode ampE = new IntCode(path);
-            Thread tE = new Thread(() -> ampE.execute(), "AMPE");
-            ampE.setInput(bridgeDE);
-            ampE.setOutput(bridgeEA);
-
-            tA.start();
-            tB.start();
-            tC.start();
-            tD.start();
-            tE.start();
-
-            try {
-                tA.join();
-                tB.join();
-                tC.join();
-                tD.join();
-                tD.join();
-            } catch (InterruptedException ie) {
-                ie.printStackTrace();
-            }
-
-            int out = bridgeEA.getAsInt();
-            if (out > max) {
-                max = out;
-            }
-        }
-        System.out.println(max);
+        System.out.println(Main.maxAmpOutputWithFeeback("org/chelonix/aoc2019/day07/input.txt"));
     }
 
-    private int runAmp(IntCode amp, int i1, int i2)  {
-        amp.setInput(new AmpInput(i1, i2));
-        AmpOutput output = new AmpOutput();
-        amp.setOutput(output);
-        amp.execute();
-        return output.getAsInt();
-    }
-
-    private class AmpInput implements IntSupplier {
-        private int[] values;
-        private int index = 0;
-
-        AmpInput(int... values) {
-            this.values = values;
-        }
-
-        @Override
-        public int getAsInt() {
-            return values[index++];
-        }
-    }
-
-    private class AmpOutput implements IntConsumer {
-        private Queue<Integer> output = new LinkedList<>();
-
-        AmpOutput() {}
-
-        @Override
-        public void accept(int value) {
-            output.add(value);
-        }
-
-        public int getAsInt() {
-            return output.remove();
-        }
-    }
-
-    private class AmpBridge implements IntSupplier, IntConsumer {
-
-        private BlockingQueue<Integer> queue = new LinkedBlockingQueue<>();
-
-        public AmpBridge(int... initialInputs) {
-            for (int x: initialInputs) {
-                queue.add(x);
-            }
-        }
-
-        @Override
-        public void accept(int value) {
-            queue.add(value);
-        }
-
-        @Override
-        public int getAsInt() {
-            try {
-                return queue.take();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-                return -1;
-            }
-        }
-    }
 }
